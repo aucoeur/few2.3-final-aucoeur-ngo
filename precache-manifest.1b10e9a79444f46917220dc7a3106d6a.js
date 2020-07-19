91ddd6440a5553047d4e5f94d6fffb07af33a9cc
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-07-18 21:20:40 -0700

    Updates

---
+++ precache-manifest.1b10e9a79444f46917220dc7a3106d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8bc059366b9523d827ef2d5ba0a45e",
+    "url": "/few2.3-final-aucoeur-ngo/index.html"
+  },
+  {
+    "revision": "f4f2e23875952a86ce41",
+    "url": "/few2.3-final-aucoeur-ngo/static/css/main.bdf48828.chunk.css"
+  },
+  {
+    "revision": "b941b2fbe824b388ba8c",
+    "url": "/few2.3-final-aucoeur-ngo/static/js/2.11360524.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/few2.3-final-aucoeur-ngo/static/js/2.11360524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f2e23875952a86ce41",
+    "url": "/few2.3-final-aucoeur-ngo/static/js/main.d7d7afc9.chunk.js"
+  },
+  {
+    "revision": "06a15b1a43d0c2f7c936",
+    "url": "/few2.3-final-aucoeur-ngo/static/js/runtime-main.67886053.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/few2.3-final-aucoeur-ngo/precache-manifest.1b10e9a79444f46917220dc7a3106d6a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/few2.3-final-aucoeur-ngo/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
